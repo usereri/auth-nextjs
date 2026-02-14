41e4503f6ebb13e00258440ae1f2e316fb443ed6
usereri <eryk.jamroz@gmail.com>
2026-02-14 18:02:02 +0100

    profile page

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,9 @@
+export default function ProfilePage() {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+            <h1>Profile Page</h1>
+            <hr />
+            <p> Profile page</p>
+        </div>
+    )
+}
