b3b4c6c8011eb235b9d71620995da403b6994160
usereri <eryk.jamroz@gmail.com>
2026-02-14 18:02:36 +0100

    DB connection

---
+++ src/dbConfig/dbConfig.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+export async function connectToDatabase() {
+    try {
+        mongoose.connect(process.env.MONGO_URL!);
+        const connection = mongoose.connection;
+
+        connection.on("connected", () => {
+            console.log("MongoDB connection established successfully");
+        })
+
+        connection.on("error", (err) => {
+            console.log("MongoDB connection error. Please make sure MongoDB is running. " + err);
+            process.exit();
+        })
+    } catch (error) {
+        console.log("Error connecting to database:", error);
+    }
+}
