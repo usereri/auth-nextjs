a56970e92eef45ad774cbd50fae1a30f5d6749e9
usereri <eryk.jamroz@gmail.com>
2026-02-14 18:01:36 +0100

    login page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import Link from "next/link";
+import React from "react";
+import { useRouter } from "next/navigation";
+import { axios } from "axios";
+
+export default function LoginPage() {
+    const [user, setUser] = React.useState({
+        email: "",
+        password: "",
+    });
+
+    const onLogin = async () => { }
+
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+            <h1>Login</h1>
+            <hr />
+            <label htmlFor="email">email</label>
+            <input
+                className="p-2 border border-grey-300 rounded-lg mb-4 focus:outline-none focus:border-grey-600"
+                id="email"
+                type="text"
+                value={user.email}
+                onChange={(e) => setUser({ ...user, email: e.target.value })}
+                placeholder="email"
+            />
+            <label htmlFor="password">password</label>
+            <input
+                className="p-2 border border-grey-300 rounded-lg mb-4 focus:outline-none focus:border-grey-600"
+                id="password"
+                type="password"
+                value={user.password}
+                onChange={(e) => setUser({ ...user, password: e.target.value })}
+                placeholder="password"
+            />
+            <button
+                className="p-2 border border-grey-300 rounded-lg mb-4 focus:outline-none focus:border-grey-600"
+                onClick={onLogin}>Login here</button>
+            <Link href="/signup">Visit signup page</Link>
+        </div>
+    )
+}
