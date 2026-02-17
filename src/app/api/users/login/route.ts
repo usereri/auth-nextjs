2f2336380d9a4cf34824ed607e537add90c474cd
usereri <eryk.jamroz@gmail.com>
2026-02-17 23:04:09 +0100

    Login logic

---
+++ src/app/api/users/login/route.ts
@@ -0,0 +1,46 @@
+import { connectToDatabase } from "@/src/dbConfig/dbConfig";
+import User from "@/src/models/userModel.js";
+import { NextRequest, NextResponse } from "next/server";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+connectToDatabase();
+
+export async function POST(request: NextRequest) {
+    try {
+        const reqBody = await request.json();
+        const { email, password } = reqBody;
+        console.log(reqBody);
+
+        // check if user exists
+        const user = await User.findOne({
+            email
+        });
+        if (!user) {
+            return NextResponse.json({ error: "User does not exist" }, { status: 400 });
+        }
+
+        // check if password is correct
+        const validPassword = await bcryptjs.compare(password, user.password);
+        if (!validPassword) {
+            return NextResponse.json({ error: "Invalid password" }, { status: 400 });
+        }
+
+        // create token data
+        const tokenData = {
+            id: user._id,
+            username: user.username,
+            email: user.email
+        }
+
+        // create token
+        const token = jwt.sign(tokenData, process.env.TOKEN_SECRET!, { expiresIn: "1h" });
+        const response = NextResponse.json({ message: "Login successful", success: true }, { status: 200 });
+        response.cookies.set("token", token, { httpOnly: true });
+
+        return response;
+
+    } catch (error: any) {
+        return NextResponse.json({ error: error.message }, { status: 500 });
+    }
+}
